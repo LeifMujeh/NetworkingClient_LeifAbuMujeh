ef1dc286c12ebd9f9a7af109844e952259df0588
LeifMujeh <Leif.abumujeh@elev.ga.ntig.se>
2024-05-20 10:34:55 +0200

    startat projektet

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+/*Client*/
+import java.io. *;
+import java.net.*;
+public class NetWorkClient{
+    public static void main(String[] args) {
+        Socket Client = null;
+
+        // Defult port Number we are going to use
+        int portnumber = 4500;
+        if (args.length >= 1) {
+
+        }
+
+
+
+    }
+}
